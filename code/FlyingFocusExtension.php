e3f9b29e8f09f5e847317fd5b27c84de0974f576
helpfulrobot <chris@silverstripe.com>
2016-01-01 04:15:37 +1300

    Converted to PSR-2

--- code/FlyingFocusExtension.php
+++ code/FlyingFocusExtension.php
@@ -1,10 +1,10 @@
 <?php
-	
-class FlyingFocusExtension extends Extension {
-	
-	public function onAfterInit() {
-		Requirements::javascript(MODULE_FLYINGFOCUS_DIR.'/javascript/flying-focus.js');
-	} 
-	
-}
 
+class FlyingFocusExtension extends Extension
+{
+    
+    public function onAfterInit()
+    {
+        Requirements::javascript(MODULE_FLYINGFOCUS_DIR.'/javascript/flying-focus.js');
+    }
+}
